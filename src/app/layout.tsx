ef5cdfc525b5aa43fa026fc11a4628b1821bb356
joaopaulosanttos <contatosantosjp@gmail.com>
2026-04-14 14:30:13 -0300

    feat: integrate Google Tag Manager (GTM) tracking into the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Funnel_Display } from "next/font/google";
 import "./globals.css";
 
@@ -62,8 +63,39 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-BR" className={`${funnelDisplay.variable}`}>
-      <body>
+    <html
+      lang="pt-BR"
+      className={`${funnelDisplay.variable}`}
+      suppressHydrationWarning
+    >
+      <head>
+        {/* GTM - Head */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','GTM-NCWN5F83');`,
+          }}
+        />
+        {/* GTM - Head */}
+      </head>
+      <body suppressHydrationWarning>
+        {/* GTM - Body */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NCWN5F83"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* GTM - Body */}
+
+        {/* Project */}
         <Header />
         {children}
         <Footer />
